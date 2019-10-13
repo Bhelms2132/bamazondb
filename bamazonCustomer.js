431121af676a047753112b1918ab4ffb33eff46e
bhelms2132 <bhelms2132@gmail.com>
2019-10-13 14:44:16 -0400

    prompt asking customer what product ID they would like to buy working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,31 +1,29 @@
 var mysql = require("mysql");
+var inquirer = require ("inquirer")
 
+// Connection information for mysql database 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "root.helms",
   database: "bamazondb"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
+  start();
 });
+// This function prompts the user for what action to take
+function start() {
+  inquirer.prompt({
+    name: "productID",
+    type: "input",
+    message: "What is the products ID that you would like to buy?",
+    choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+  })
+  .then(function(answer){
 
-function afterConnection() {
-  connection.query("SELECT * FROM bamazondb.products", function(err, res) {
-    if (err) throw err;
-    console.log(res);
-    
-    connection.end();
   });
 }
 
