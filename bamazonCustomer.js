4e1b0571edd5ca5e7aacab166dbf322a703c1017
bhelms2132 <bhelms2132@gmail.com>
2019-10-14 18:15:08 -0400

    start function asking for product ID to purchase working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,24 +9,38 @@ var connection = mysql.createConnection({
   password: "root.helms",
   database: "bamazondb"
 });
-
 connection.connect(function(err) {
   if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  connection.end();
+});
+connection.query("SELECT * FROM bamazondb.products", function(err, res){
+  if (err) throw err;
+  console.log(res);
   start();
 });
 // This function prompts the user for what action to take
 function start() {
   inquirer.prompt({
-    name: "productID",
+    name: "productId",
     type: "input",
     message: "What is the products ID that you would like to buy?",
-    choices: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
   })
   .then(function(answer){
-
+    console.log("ID: " + answer.productId);
+    
   });
 }
-
+// function to handle products to purchase
+function units() {
+   inquirer.prompt({
+     name:"units",
+     type:"input",
+     message: "How many units of the product would you like to buy?"
+   });
+   
+   
+}
 
 
 
